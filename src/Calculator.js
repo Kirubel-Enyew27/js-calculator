38fafa1a651f2ea56664262702ad66654ae55c93
Kirubel-Enyew27 <enyewkirubel76@gmail.com>
2023-12-19 22:13:33 +0300

    second commit

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import { evaluate } from 'mathjs';  // Import the 'evaluate' function directly
 
 export default function Calculator() {
   const [dataValue, setDataValue] = useState({
@@ -11,14 +12,29 @@ export default function Calculator() {
     setDataValue((prevData) => {
       let newValue;
 
+      if (value === 'clr') {
+        // Clear button pressed
+        return {
+          numberValue: [],
+          stringValue: '',
+        };
+      }
+
       // Handle different types of input
       if (typeof value === 'number' || value === '.') {
         // If the input is a number or a decimal point, append it to the array
         newValue = [...prevData.numberValue, value];
       } else {
-        // If the input is an operator or a special character, append it to the array
+        // If the input is an operator, handle the previous operator and operands
         const lastElement = prevData.numberValue.length > 0 ? prevData.numberValue[prevData.numberValue.length - 1] : null;
-        newValue = lastElement === null || typeof lastElement === 'number' ? [...prevData.numberValue, value] : prevData.numberValue;
+
+        if (lastElement === null || typeof lastElement === 'number') {
+          newValue = [...prevData.numberValue, value];
+        } else {
+          // Replace the last operator with the new one
+          const updatedValue = [...prevData.numberValue.slice(0, -1), value];
+          newValue = updatedValue;
+        }
       }
 
       return {
@@ -30,6 +46,7 @@ export default function Calculator() {
   };
 
   const handleClearButton = () => {
+    // Clear button pressed
     setDataValue({
       numberValue: [],
       stringValue: '',
@@ -37,52 +54,18 @@ export default function Calculator() {
   };
 
   const handleEqualsButton = () => {
-    let input = dataValue.numberValue;
-
-    let operand1 = '';
-    let operator = '';
-    let operand2 = '';
-    let result;
-
-    input.forEach((element) => {
-      if (typeof element === 'number' || element === '.') {
-        if (operator === '') {
-          operand1 += element;
-        } else {
-          operand2 += element;
-        }
-      } else {
-        operator += element;
-      }
-    });
-
-    operand1 = parseFloat(operand1);
-    operand2 = parseFloat(operand2);
-
-    switch (operator) {
-      case '+':
-        result = operand1 + operand2;
-        break;
-      case '-':
-        result = operand1 - operand2;
-        break;
-      case 'x':
-        result = operand1 * operand2;
-        break;
-      case '÷':
-        // Check for division by zero
-        result = operand2 !== 0 ? operand1 / operand2 : 'Error: Division by zero';
-        break;
-      default:
-        // If no operator is present, return the last operand as the result
-        result = operand1;
-        break;
+    try {
+      const result = evaluate(dataValue.stringValue);  // Use the 'evaluate' function
+      setDataValue({
+        numberValue: [result],
+        stringValue: result.toString(),
+      });
+    } catch (error) {
+      setDataValue({
+        numberValue: [],
+        stringValue: 'Error',
+      });
     }
-
-    setDataValue({
-      numberValue: [],
-      stringValue: result.toString(),
-    });
   };
 
   return (
@@ -92,9 +75,9 @@ export default function Calculator() {
       </div>
 
       <div className="button-grid">
-        {[1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, 'x', '.', 0, 'clr', '÷'].map((button, index) => (
-          <button key={index} onClick={button==='clr' ? handleClearButton: () => handleClick(button)} className={button === 'clr' ? 'clear-button' : (typeof button === 'number' ? 'number-button' : 'operator-button')}>
-            {button === 'clr' ? 'Clear' : button}
+        {[1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', '.', 0, 'clr', '/'].map((button, index) => (
+          <button key={index} onClick={() => handleClick(button)} className={button === 'clr' ? 'clear-button' : (typeof button === 'number' ? 'number-button' : 'operator-button')}>
+            {button === 'clr' ? 'C' : button}
           </button>
         ))}
       </div>
